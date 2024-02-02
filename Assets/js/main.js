d3f6e3a42d4cd4449a49b89ed4b640d00f4ab6e8
Cristian Contreras <cristian.fca01@gmail.com>
2024-02-02 01:01:50 -0300

    Se agrega carrousel e implenta navbar transparente.

---
+++ Assets/js/main.js
@@ -0,0 +1,11 @@
+//Cambia de color del navbar al bajar al fondo de la página//
+window.onscroll = function (e) {  
+    var element = document.getElementById("menu");
+    if(scrollY >= 400){
+        element.classList.remove("bg-transparent");
+        element.classList.add("bg-dark");
+    } else {
+        element.classList.remove("bg-dark");
+        element.classList.add("bg-transparent");
+    }
+    } 
