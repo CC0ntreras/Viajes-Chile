5f6053cb9bba481c7342a3b98b19ddb752847874
Cristian Contreras <cristian.fca01@gmail.com>
2024-02-04 15:17:31 -0300

    Se corrigen detalles finales de estilo y errores detectados en JS

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,13 +1,25 @@
-    $(document).ready(function () {
+// Smooth Scrolling al clickear elementos de Navbar
+$(document).ready(function () {
         $("a").on('click', function (event) {
             if (this.hash !== "") {
                 event.preventDefault();
                 var hash = this.hash;
                 $('html, body').animate({
                     scrollTop: $(hash).offset().top
-                }, 2500, function () {
+                }, 1500, function () {
                     window.location.hash = hash;
                 });
             }
+
+// Mensaje de alerta al clikear "enviar"
+        $("#submit").click(function (e) {
+            e.preventDefault();
+            alert("¡Envio Satisfactorio del correo!");
+            });
         });
+        
+// Script para tooltips       
+        document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach((tooltip) => {
+            new bootstrap.Tooltip(tooltip);
+          });
     });
